2fcb2bc5087761d15c9afa4dd621be742f36effa
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-03-18 04:47:14 -0700

    footer import

---
+++ react-front-end/src/components/Footer.js
@@ -0,0 +1,23 @@
+import './assets/css/footer.css'
+const Footer = () => {
+
+    return(
+     
+        <footer className="section-footer border-top">
+            <div className="container-fluid">
+                <section className="footer-top padding-y">
+                    <div className="row">
+                        <aside className="col-md-6">
+                            <article className="mr-3">
+                                <p className="mt-3 description">Some short text about company like You might remember the Dell computer commercials in which a youth reports this exciting news to his friends.</p>
+                                <div> <a className="btn btn-icon btn-light" title="Facebook" target="_blank" href="#" data-abc="true"><i className="fab fa-facebook-f"></i></a> <a className="btn btn-icon btn-light" title="Instagram" target="_blank" href="#" data-abc="true"><i className="fab fa-instagram"></i></a> <a className="btn btn-icon btn-light" title="Youtube" target="_blank" href="#" data-abc="true"><i className="fab fa-youtube"></i></a> <a className="btn btn-icon btn-light" title="Twitter" target="_blank" href="#" data-abc="true"><i className="fab fa-twitter"></i></a> </div>
+                            </article>
+                        </aside>
+                        <aside className="col-sm-4 col-md-2">
+                            <h6 className="title">About</h6>
+                            <ul className="list-unstyled">
+                                <li> <a href="#" data-abc="true">About us</a></li>
+                                <li> <a href="#" data-abc="true">Services</a></li>
+                                <li> <a href="#" data-abc="true">Terms & Condition</a></li>
+                                <li> <a href="#" data-abc="true">Our Blogs</a></li>
+                            </ul>
