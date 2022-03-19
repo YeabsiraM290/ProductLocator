2cae4a481bc43667cd5a1fdca7bfed5622b0557e
yeabsiraM290 <yeabsiraM@outlook.com>
2022-03-19 17:28:39 +0300

    forget password page

---
+++ react-front-end/src/components/ForgetPassword.js
@@ -0,0 +1,27 @@
+const ForgetPass = () => {
+
+    return(
+
+        <div className="container text-center login-container mb-5">
+
+            <p className="login-title"> Password assistance </p>
+
+            <br/>
+
+            <p className="small-text">Enter the email address associated with your account.</p>
+            
+            <form>
+
+                <div className="form-outline mb-4">
+                    <input type="email" id="email" placeholder="email" className="form-control" />
+                </div>
+
+                <button type="submit" className="login-btn btn btn-primary btn-block mb-4">Submit</button>
+
+            </form>
+            
+        </div>
+    )
+}
+
+export default ForgetPass
