e0c602ec1206f0059f9547e37873abe1c5efb219
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-03-20 02:05:39 -0700

    header

---
+++ react-front-end/src/components/Header.js
@@ -0,0 +1,14 @@
+import './assets/css/header.css'
+
+const Header = () =>{
+
+    return(
+<div className="text-center header">
+  
+      <p className="logo">Product Locator</p>
+  
+  </div>
+    )
+}
+
+export default Header
