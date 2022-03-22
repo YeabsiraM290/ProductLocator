f0ac19735ff9f999d79273bd43b3fce47441874d
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-03-22 04:09:00 -0700

    map import

---
+++ react-front-end/src/components/Map.js
@@ -0,0 +1,27 @@
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+const Map = () => {
+
+    return(
+
+        <div className="row mt-3 item-control">
+      
+            
+
+            <div className="col-7">
+
+ 
+                </div>
+            <div className="col-5">
+
+            <div className="row  mb-4">
+    <div className="col-6"><img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+      
+ 
+     </div>
+     
+
+      <div className="col-6">
+      Dell xps15<br />
+      200,000<br />
+      Dell xps15<br />
