87615c9e00307a8821b6ff917eb4b8bceebfcb5c
ermias19 <ermiasmulu19@gmail.com>
2022-03-22 09:00:37 +0300

    the profile item  of the vendor is created.

---
+++ react-front-end/components/vendor/VendorItemProfile.js
@@ -0,0 +1,261 @@
+import "../assets/css/cart.css";
+
+const VendorItemControl = () => {
+
+    return(
+
+        <div className="row mt-3 item-control">
+        <a className="add-item" href="#">Add new item</a>
+            
+
+            <div className="col-5 items-list">
+            <p className="title text-center">Your Products</p>
+            <div className="row cartRow mb-4">
+    <div className="col-6"><img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+      
+ 
+     </div>
+     
+
+      <div className="col-6">
+      Dell xps15<br />
+      200,000<br />
+      Dell xps15<br />
+      <span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star"></span>
+<span className="fa fa-star"></span>
+<span className="count">500</span>
+       
+        
+      </div>
+  
+  </div>
+
+  <div className="row cartRow mb-4">
+    <div className="col-6"><img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+      
+ 
+     </div>
+     
+
+      <div className="col-6">
+      Dell xps15<br />
+      200,000<br />
+      Dell xps15<br />
+      <span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star"></span>
+<span className="fa fa-star"></span>
+<span className="count">500</span>
+       
+        
+      </div>
+  
+  </div>
+  <div className="row cartRow mb-4">
+    <div className="col-6"><img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+      
+ 
+     </div>
+     
+
+      <div className="col-6">
+      Dell xps15<br />
+      200,000<br />
+      Dell xps15<br />
+      <span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star"></span>
+<span className="fa fa-star"></span>
+<span className="count">500</span>
+       
+        
+      </div>
+  
+  </div>
+
+  <div className="row cartRow mb-4">
+    <div className="col-6"><img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+      
+ 
+     </div>
+     
+
+      <div className="col-6">
+      Dell xps15<br />
+      200,000<br />
+      Dell xps15<br />
+      <span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star"></span>
+<span className="fa fa-star"></span>
+<span className="count">500</span>
+       
+        
+      </div>
+  
+  </div>
+
+  <div className="row cartRow mb-4">
+    <div className="col-6"><img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+      
+ 
+     </div>
+     
+
+      <div className="col-6">
+      Dell xps15<br />
+      200,000<br />
+      Dell xps15<br />
+      <span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star"></span>
+<span className="fa fa-star"></span>
+<span className="count">500</span>
+       
+        
+      </div>
+  
+  </div>
+
+  <div className="row cartRow mb-4">
+    <div className="col-6"><img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+      
+ 
+     </div>
+     
+
+      <div className="col-6">
+      Dell xps15<br />
+      200,000<br />
+      Dell xps15<br />
+      <span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star"></span>
+<span className="fa fa-star"></span>
+<span className="count">500</span>
+       
+        
+      </div>
+  
+  </div>
+
+                
+                
+                </div>
+            <div className="col-7">
+
+
+            <div>
+
+<div className="container text-center">
+
+<p className="text-center main-title"> Item information </p>
+     
+        <form>
+
+        <div className="form-outline mb-4">
+
+<div className="row">
+
+  <div className="col-3">
+  <label for="shop_img">Item name</label>
+  </div>
+
+  <div className="col-9">
+  <div className="form-outline mb-4">
+                <input type="text" id="form2Example1" value="PC" className="form-control" />
+
+            </div>
+  </div>
+</div>
+
+</div>
+
+<div className="form-outline mb-4">
+
+<div className="row">
+
+  <div className="col-3">
+  <label for="shop_img">Discription</label>
+  </div>
+
+  <div className="col-9">
+  <div className="form-outline mb-4">
+                <textarea type="number" id="form2Example1" value="Discription" className="form-control" />
+
+            </div>
+  </div>
+</div>
+
+</div>
+
+<div className="form-outline mb-4">
+
+<div className="row">
+
+  <div className="col-3">
+  <label for="shop_img">Discription</label>
+  </div>
+
+  <div className="col-9">
+  <div className="form-outline mb-4">
+                <input type="number" id="form2Example1" placeholder="Price" value="2000" className="form-control" />
+
+            </div>
+  </div>
+</div>
+
+</div>
+
+   
+
+
+      
+
+    
+
+            <div className="form-outline mb-4">
+
+              <div className="row">
+
+                <div className="col-3">
+                <label for="shop_img">Item image</label>
+                </div>
+
+                <div className="col-9 img-view">
+                <img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+                <img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+                <img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+                <img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+                </div>
+              </div>
+          
+            </div>
+
+      
+         
+
+            <button type="button" className="login-btn btn btn-primary btn-block mb-4">Save</button>
+
+        </form>
+    </div>
+
+    </div>
+                
+                </div>
+
+        </div>
+    )
+
+
+} 
+
+export default VendorItemControl
