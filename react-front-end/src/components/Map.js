0913452cd9f56516b5c3d7ad097776b6e3340972
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-03-23 02:42:20 -0700

    map export

--- react-front-end/src/components/Map.js
+++ react-front-end/src/components/Map.js
@@ -99,3 +99,62 @@ const Map = () => {
 <span className="fa fa-star"></span>
 <span className="fa fa-star"></span>
 <span className="count">500</span>
+
+</div>
+  
+  </div>
+
+  <div className="row  mb-4">
+    <div className="col-6"><img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+      
+ 
+     </div>
+     
+
+      <div className="col-6">
+      Dell xps15<br />
+      200,000<br />
+      Dell xps15<br />
+      <span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star"></span>
+<span className="fa fa-star"></span>
+<span className="count">500</span>
+       
+        
+      </div>
+  
+  </div>
+
+  <div className="row  mb-4">
+    <div className="col-6"><img className="img"  src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=christian-wiediger-WkfDrhxDMC8-unsplash.jpg" alt="First slide" width="200px" height="100px" />
+      
+ 
+     </div>
+     
+
+      <div className="col-6">
+      Dell xps15<br />
+      200,000<br />
+      Dell xps15<br />
+      <span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star checked"></span>
+<span className="fa fa-star"></span>
+<span className="fa fa-star"></span>
+<span className="count">500</span>
+       
+        
+      </div>
+  
+  </div>    
+                
+                </div>
+
+        </div>
+    )
+
+}
+
+export default Map
