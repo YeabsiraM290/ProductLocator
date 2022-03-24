40786bbca581a9018da2ec9bcde8115852ba54bc
TekletsadikAbebayehuTirfe <it.tekletsadik.abebayehutirfe@gmail.com>
2022-03-24 09:06:28 +0300

    made some changes on the admin and views

--- back-end/main/admin.py
+++ back-end/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models.Account import *
 
+admin.site.register(Vendor)
+admin.site.register(VendorMore)
 # Register your models here.
--- back-end/main/views.py
+++ back-end/main/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .permissions import *
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    # permission_classes = [PostUserWritePermission]
+
+class VendorViewSet(viewsets.ModelViewSet):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+    # permission_classes = [PostUserWritePermission]
