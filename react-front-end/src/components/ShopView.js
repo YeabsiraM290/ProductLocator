40fd3df960ddfae5d9be36d54147e6dd74998cfd
yeabsiraM290 <yeabsiraM@outlook.com>
2022-03-25 17:32:23 +0300

    shop view page

---
+++ react-front-end/src/components/ShopView.js
@@ -0,0 +1,40 @@
+import ItemCard from './ItemCard'
+import './assets/css/shopView.css'
+
+const ShopView = () => {
+
+    return(
+
+        <div className='shop_view_items'>
+            <p className='text-center shop-name mt-5 mb-4'>Shop name</p>
+               <div className='row mb-5 mt-2 no-gutter'>
+
+<div className='col-md'>{ItemCard()}</div>
+<div className='col-md'>{ItemCard()}</div>
+<div className='col-md'>{ItemCard()}</div>
+<div className='col-md'>{ItemCard()}</div>
+<div className='col-md'>{ItemCard()}</div>
+</div>
+<div className='row mb-5'>
+
+<div className='col'>{ItemCard()}</div>
+<div className='col'>{ItemCard()}</div>
+<div className='col'>{ItemCard()}</div>
+<div className='col'>{ItemCard()}</div>
+<div className='col'>{ItemCard()}</div>
+</div>
+
+<div className='row mb-5'>
+
+<div className='col'>{ItemCard()}</div>
+<div className='col'>{ItemCard()}</div>
+<div className='col'>{ItemCard()}</div>
+<div className='col'>{ItemCard()}</div>
+<div className='col'>{ItemCard()}</div>
+</div>
+        </div>
+    
+    )
+}
+
+export default ShopView
