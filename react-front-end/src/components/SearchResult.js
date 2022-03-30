d9145914d6777aae7f8ac68fa1ed4f0fb1333ea3
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-03-30 15:11:05 -0700

    searchresult

---
+++ react-front-end/src/components/SearchResult.js
@@ -0,0 +1,31 @@
+const SearchResult = () => {
+
+    return(
+
+        <div className="row mt-3 item-control">
+      
+            
+
+            <div className="col-4">
+
+      
+
+<div className="card">
+	<article className="card-group-item">
+		<header className="card-header">
+			<h6 className="title">Range input </h6>
+		</header>
+		<div className="filter-content">
+			<div className="card-body">
+			<div className="form-row">
+			<div className="form-group col-md-6">
+			  <label>Min</label>
+			  <input type="number" className="form-control" id="inputEmail4" placeholder="$0" />
+			</div>
+			<div className="form-group col-md-6 text-right">
+			  <label>Max</label>
+			  <input type="number" className="form-control" placeholder="$1,0000" />
+			</div>
+			</div>
+			</div> 
+		</div>
