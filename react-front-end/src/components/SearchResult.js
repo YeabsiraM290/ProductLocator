8fb8e27cf0cf3d856eb2eba4e64e268a98fbc391
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-03-31 06:02:12 -0700

    searchresult form

--- react-front-end/src/components/SearchResult.js
+++ react-front-end/src/components/SearchResult.js
@@ -29,3 +29,29 @@ const SearchResult = () => {
 			</div>
 			</div> 
 		</div>
+
+
+        </article> 
+	<article className="card-group-item">
+		<header className="card-header">
+			<h6 className="title">Selection </h6>
+		</header>
+		<div className="filter-content">
+			<div className="card-body">
+				<div className="custom-control custom-checkbox">
+					<span className="float-right badge badge-light round">52</span>
+				  	<input type="checkbox" className="custom-control-input" id="Check1" />
+				  	<label className="custom-control-label" for="Check1">Samsung</label>
+				</div> 
+
+				<div className="custom-control custom-checkbox">
+					<span className="float-right badge badge-light round">132</span>
+				  	<input type="checkbox" className="custom-control-input" id="Check2" />
+				 	<label className="custom-control-label" for="Check2">Black berry</label>
+				</div> 
+
+				<div className="custom-control custom-checkbox">
+					<span className="float-right badge badge-light round">17</span>
+				  	<input type="checkbox" className="custom-control-input" id="Check3" />
+				  	<label className="custom-control-label" for="Check3">Samsung</label>
+				</div>
