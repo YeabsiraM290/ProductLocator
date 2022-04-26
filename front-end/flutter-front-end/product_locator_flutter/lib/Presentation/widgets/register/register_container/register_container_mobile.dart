a6a09347cc4b1e30b419dc1725c43badbede1c25
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-04-26 03:24:34 -0700

    register container mobile

---
+++ front-end/flutter-front-end/product_locator_flutter/lib/Presentation/widgets/register/register_container/register_container_mobile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:product_locator_flutter/Presentation/widgets/product_locator_Home_cards/cards_content/cards_content_mobile.dart';
+import 'package:product_locator_flutter/Presentation/widgets/profile_view_widget/profile_conatiner/profile_container.dart';
+import 'package:product_locator_flutter/Presentation/widgets/register/register_container/register_container.dart';
+// import 'package:product_locator_flutter/widgets/profile_view_widget/profile_conatiner/profile_container.dart';
+// import 'package:product_locator_flutter/widgets/register/register_container/register_container.dart';
+// import 'package:product_locator_flutter/widgets/product_locator_Home_cards/cards_content/cards_content_mobile.dart';
+
+// import 'package:product_locator_flutter/widgets/product_locator_Home_cards/cards_container/cards_container.dart';
+
+// import 'package:product_locator_flutter/widgets/shop/shop_container/shop_container.dart';
+
+class RegisterContainerMobile extends StatelessWidget {
+  const RegisterContainerMobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: double.infinity,
+        child: Column(children: [
+          const HomeTitle(),
+          Column(
+            children: RegisterContainer.getRegister(),
+          ),
+        ]));
+  }
+}
+
+class CardContainerLandscape extends StatelessWidget {
+  const CardContainerLandscape({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: ProfileContainer.getCardsContent());
+  }
+}
