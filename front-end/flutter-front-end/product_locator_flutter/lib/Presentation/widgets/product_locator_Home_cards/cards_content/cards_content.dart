94a736a4f78b2a8b48b8291966de4c8b7ae7cb6c
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-04-27 08:06:02 -0700

    card content

---
+++ front-end/flutter-front-end/product_locator_flutter/lib/Presentation/widgets/product_locator_Home_cards/cards_content/cards_content.dart
@@ -0,0 +1,51 @@
+import 'package:product_locator_flutter/Presentation/responsive/Screen_type.dart';
+import 'package:product_locator_flutter/Presentation/widgets/product_locator_Home_cards/cards_content/cards_content_mobile.dart';
+// import 'package:product_locator_flutter/responsive/Screen_type.dart';
+import 'package:flutter/material.dart';
+// import 'package:product_locator_flutter/responsive/orietation_layout.dart';
+// import 'package:product_locator_flutter/widgets/product_locator_Home_cards/cards_content/cards_content_mobile.dart';
+// import 'package:product_locator_flutter/widgets/product_locator_Home_cards/cards_content/cards_content_tablet.dart';
+
+class ProductContent extends StatelessWidget {
+  // final double? Width;
+  final Color? color;
+  final Image image;
+  final Image? detail;
+  final Image? playbutton;
+  final Image? progressindicator;
+  final Image? title;
+  final Image? view;
+  const ProductContent(
+      {Key? key,
+      this.color,
+      required this.image,
+      this.detail,
+      this.playbutton,
+      this.progressindicator,
+      this.title,
+      this.view})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout(
+      mobile: ProductContentMobilePortrait(
+          color: color,
+          image: image,
+          detail: detail,
+          playbutton: playbutton,
+          progressindicator: progressindicator,
+          title: title,
+          view: view),
+      // tablet: OrientationLayout(
+      //     portrait: CardsContentTabletPortrait(
+      //         color: color,
+      //         image: image,
+      //         detail: detail,
+      //         playbutton: playbutton,
+      //         progressindicator: progressindicator,
+      //         title: title,
+      //         view: view)),
+    );
+  }
+}
