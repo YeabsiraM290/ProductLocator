a5fda7a0001b14afeb790ecac0c8d52729312aa1
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-06-15 21:20:49 -0700

    profile model

--- front-end/flutter-front-end/product_locator_flutter/lib/Data/Model/profile_model/profile_model.dart
+++ front-end/flutter-front-end/product_locator_flutter/lib/Data/Model/profile_model/profile_model.dart
@@ -3,23 +3,25 @@ import 'package:json_annotation/json_annotation.dart';
 
 class ProfileModel {
   int id;
-  String firstname;
+  String username;
   String email;
   int phone_number;
-  String image;
-  ProfileModel(
-      {required this.id,
-      required this.firstname,
-      required this.email,
-      required this.phone_number,
-      required this.image});
+  // String image;
+  ProfileModel({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.phone_number,
+    // required this.image
+  });
 
   factory ProfileModel.fromJson(Map<String, dynamic> json) {
     return ProfileModel(
-        id: json['id'],
-        firstname: json['first_name'],
-        email: json['email'],
-        phone_number: json['phone_no'],
-        image: json['profile_pic']);
+      id: json['id'],
+      username: json['username'],
+      email: json['email'],
+      phone_number: json['phone_no'],
+      // image: json['profile_pic']
+    );
   }
 }
